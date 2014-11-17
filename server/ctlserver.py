82795eddbe6d650d4e01846111030743e68475a8
tlhc <marst629@gmail.com>
2014-11-17 17:47:14 +0000

    add VideoProcessMng

--- server/ctlserver.py
+++ server/ctlserver.py
@@ -5,11 +5,13 @@
 
 import os
 import sys
+import cgi
 import signal
 import socket
 import threading
 import netifaces
 import SocketServer
+import BaseHTTPServer
 import logging
 import subprocess
 
@@ -18,14 +20,14 @@ def signal_handler(signals, frame):
     """ handle ctrl -c """
     _, _ = signals, frame
     APPLOGGER.info('server exiting..')
-    ThreadedTCPRequestHandler.process_lock.acquire()
+    vvmng = VideoProcessMng()
+    vvmng.getlock()
     try:
-        if ThreadedTCPRequestHandler.vvprocess is not None:
-            if ThreadedTCPRequestHandler.vvprocess.poll() is None:
-                os.killpg(ThreadedTCPRequestHandler.vvprocess.pid,
-                          signal.SIGTERM)
+        if vvmng.isset():
+            if vvmng.isrun():
+                os.killpg(vvmng.pid(), signal.SIGTERM)
     finally:
-        ThreadedTCPRequestHandler.process_lock.release()
+        vvmng.releaselock()
 
     APPLOGGER.info('shutdown complete')
     sys.exit(0)
@@ -88,73 +90,121 @@ class RaspvidCmd(object):
         cmdstr += str(self.rtsp_port) + "/}' :demux=h264"
         return cmdstr
 
+
+class Singleton(type):
+    """ Singleton """
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class VideoProcessMng(object):
+    """ VideoProcessMng ensure one video process in thread """
+    __metaclass__ = Singleton
+    def __init__(self):
+        self.vvprocess = None
+        self.__process_lock = threading.Lock()
+        self.process_cmd = RaspvidCmd()
+    def getlock(self):
+        """ get lock """
+        self.__process_lock.acquire()
+    def releaselock(self):
+        """ release lock """
+        self.__process_lock.release()
+    def setprocess(self, process):
+        """ set process """
+        self.vvprocess = process
+    def isset(self):
+        """ is set or not """
+        if self.vvprocess is not None:
+            return True
+        return False
+    def isrun(self):
+        """ is run or not """
+        if self.vvprocess is not None and self.vvprocess.poll() is None:
+            return True
+        return False
+    def pid(self):
+        """ return pid """
+        return self.vvprocess.pid
+    def start(self):
+        """ start video process """
+        APPLOGGER.info('subcall in porcess')
+        child = None
+        child = subprocess.Popen(self.process_cmd.cmd(),
+                                 shell=True, preexec_fn=os.setsid)
+        if child is not None:
+            self.vvprocess = child
+            APPLOGGER.info('video process start and set')
+    def stop(self):
+        """ kill video process """
+        if self.isset() and self.isrun():
+            os.killpg(self.vvprocess.pid, signal.SIGTERM)
+            APPLOGGER.info('send video process stoped signal')
+
+
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     """ TCPServer RequestHandler """
-    vvprocess = None
-    process_lock = threading.Lock()
     def __init__(self, request, client_address, server):
         self.maxbuf = 2048
-        self.maxthr = 4
-        self.raspcmd = RaspvidCmd()
+        self.vvpmng = VideoProcessMng()
         self.clientcmd_start = "start"
         self.clientcmd_stop = "stop"
         SocketServer.BaseRequestHandler.__init__(self, request,
                                                  client_address, server)
 
     def handle(self):
-        if threading.activeCount() > self.maxthr:
-            APPLOGGER.warn('theading number exceeded')
-            return
+        APPLOGGER.info('theading number is ' + str(threading.activeCount()))
         data = self.request.recv(self.maxbuf)
         self.__process_req(data)
 
     def __start_process(self):
         """ start video process """
-        ThreadedTCPRequestHandler.process_lock.acquire()
+        self.vvpmng.getlock()
         try:
-            if ThreadedTCPRequestHandler.vvprocess is None:
-                self.__sub_call(self.raspcmd.cmd())
-                self.request.sendall(self.raspcmd.cmd())
+            if not self.vvpmng.isset():
+                self.vvpmng.start()
+                self.request.sendall(self.vvpmng.process_cmd.cmd())
                 APPLOGGER.info("video server run.")
             else:
-                if ThreadedTCPRequestHandler.vvprocess.poll() is None:
+                if self.vvpmng.isrun():
                     APPLOGGER.info('already run subprocess: ' +
-                                   str(ThreadedTCPRequestHandler.vvprocess.pid))
+                                   str(self.vvpmng.pid()))
                     APPLOGGER.info("video process already run.")
                     self.request.sendall(self.clientcmd_start + '|' + '1')
-                    self.request.sendall(self.raspcmd.cmd())
+                    self.request.sendall(self.vvpmng.process_cmd.cmd())
                 else:
                     APPLOGGER.info('subprocess not running')
             APPLOGGER.info('activeCount is ' + str(threading.activeCount()))
         finally:
-            ThreadedTCPRequestHandler.process_lock.release()
+            self.vvpmng.releaselock()
 
     def __stop_process(self):
         """ __stop_process """
-        ThreadedTCPRequestHandler.process_lock.acquire()
+        self.vvpmng.getlock()
         try:
-            if ThreadedTCPRequestHandler.vvprocess is None:
+            if not self.vvpmng.isset():
                 APPLOGGER.warn('no process to stop')
                 self.request.sendall(self.clientcmd_stop + '|' + '0')
                 return
-            if ThreadedTCPRequestHandler.vvprocess.poll() is None:
-                os.killpg(ThreadedTCPRequestHandler.vvprocess.pid,
-                          signal.SIGTERM)
-                # ThreadedTCPRequestHandler.vvprocess.terminate()
+            if self.vvpmng.isrun():
+                self.vvpmng.stop()
                 APPLOGGER.warn('terminating..')
-                ThreadedTCPRequestHandler.vvprocess = None
+                self.vvpmng.setprocess(None)
                 self.request.sendall(self.clientcmd_stop + '|' + '1') # fake done
             else:
                 APPLOGGER.info('process is terminate')
-                ThreadedTCPRequestHandler.vvprocess = None
+                self.vvpmng.setprocess(None)
                 self.request.sendall(self.clientcmd_stop + '|' + '0')
         finally:
-            ThreadedTCPRequestHandler.process_lock.release()
+            self.vvpmng.releaselock()
 
     def __sysinfo(self):
         """ for get cmd """
         ipaddr, _ = self.server.server_address
-        vport = self.raspcmd.rtsp_port
+        vport = self.vvpmng.process_cmd.rtsp_port
         self.request.sendall(str(ipaddr) + ':' + str(vport))
 
     def __changevprocss(self, data):
@@ -173,30 +223,29 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
             return
 
         if 'brightness' in paradict:
-            self.raspcmd.bright = int(paradict['brightness'])
+            self.vvpmng.process_cmd.bright = int(paradict['brightness'])
         if 'bitrate' in paradict:
-            self.raspcmd.bitrate = int(paradict['bitrate'])
+            self.vvpmng.process_cmd.bitrate = int(paradict['bitrate'])
         if 'fps' in paradict:
-            self.raspcmd.fps = int(paradict['fps'])
+            self.vvpmng.process_cmd.fps = int(paradict['fps'])
         if 'height' in paradict:
-            self.raspcmd.height = int(paradict['height'])
+            self.vvpmng.process_cmd.height = int(paradict['height'])
         if 'width' in paradict:
-            self.raspcmd.width = int(paradict['width'])
+            self.vvpmng.process_cmd.width = int(paradict['width'])
 
-        ThreadedTCPRequestHandler.process_lock.acquire()
+        self.vvpmng.getlock()
         try:
-            if ThreadedTCPRequestHandler.vvprocess is None:
-                self.__sub_call(self.raspcmd.cmd())
+            if not self.vvpmng.isset():
+                self.vvpmng.start()
                 return
-            if ThreadedTCPRequestHandler.vvprocess.poll() is None:
-                os.killpg(ThreadedTCPRequestHandler.vvprocess.pid,
-                          signal.SIGTERM)
-                ThreadedTCPRequestHandler.vvprocess = None
-                self.__sub_call(self.raspcmd.cmd())
+            if self.vvpmng.isrun():
+                self.vvpmng.stop()
+                self.vvpmng.setprocess(None)
+                self.vvpmng.start()
             else:
-                self.__sub_call(self.raspcmd.cmd())
+                self.vvpmng.start()
         finally:
-            ThreadedTCPRequestHandler.process_lock.release()
+            self.vvpmng.releaselock()
 
     def __process_req(self, data):
         """ process req """
@@ -225,7 +274,7 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     """ TCPServer """
-    pass
+    allow_reuse_address = True
 
 
 def __get_local_ip():
@@ -233,29 +282,173 @@ def __get_local_ip():
     ipaddr = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
     return ipaddr
 
-def tcpserve():
+def tcpserve(ipaddr, serve_port):
     """ tcpserve """
-    ipaddr = ''
     try:
-        ipaddr = __get_local_ip()
         if ipaddr is '':
             raise AppException('get local ip exp')
+        if int(serve_port) <= 0 or int(serve_port) > 65535:
+            raise AppException('port num err')
     except AppException as ex:
         APPLOGGER.error(ex)
 
-    host, port = ipaddr, 9999
+    host, port = ipaddr, int(serve_port)
     server = None
     try:
         server = ThreadedTCPServer((host, port), ThreadedTCPRequestHandler)
     except socket.error as ex:
         APPLOGGER.error(ex)
         sys.exit(1)
-    APPLOGGER.info('Server Up')
+    APPLOGGER.info('Server Up IP=%s PORT=%s', ipaddr, serve_port)
     if server:
         server.serve_forever()
     else:
         raise AppException('server start err')
 
+
+class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """ HttpHandler for GET and POST """
+    def do_GET(self):
+        """ GET """
+        if self.path == "/":
+            self.path = "/index.html"
+        try:
+            # Check the file extension required and
+            # set the right mime type
+            sendreply = False
+            if self.path.endswith(".html"):
+                mimetype = 'text/html'
+                sendreply = True
+            if self.path.endswith(".jpg"):
+                mimetype = 'image/jpg'
+                sendreply = True
+            if self.path.endswith(".gif"):
+                mimetype = 'image/gif'
+                sendreply = True
+            if self.path.endswith(".js"):
+                mimetype = 'application/javascript'
+                sendreply = True
+            if self.path.endswith(".css"):
+                mimetype = 'text/css'
+                sendreply = True
+
+            if sendreply == True:
+                # Open the static file requested and send it
+                fhandler = open(os.curdir + os.sep + self.path)
+                self.send_response(200)
+                self.send_header('Content-type', mimetype)
+                self.end_headers()
+                self.wfile.write(fhandler.read())
+                fhandler.close()
+            return
+
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        """ POST """
+        APPLOGGER.debug(self.path)
+        _environ = {'REQUEST_METHOD': 'POST',
+                    'CONTENT_TYPE': self.headers['Content-Type'],}
+        form = cgi.FieldStorage(fp=self.rfile,
+                                headers=self.headers,
+                                environ=_environ)
+        if self.path == "/start":
+            APPLOGGER.debug(str(form))
+            self.send_response(200)
+            self.end_headers()
+            return
+        elif self.path == '/stop':
+            APPLOGGER.debug(str(form))
+            self.send_response(200)
+            self.end_headers()
+            return
+        elif self.path == '/change':
+            APPLOGGER.debug(str(form))
+            self.send_response(200)
+            self.end_headers()
+            return
+        else:
+            APPLOGGER.debug(str(form))
+            self.send_response(503)
+            self.end_headers()
+            return
+
+
+class ThreadedHTTPServer(SocketServer.ThreadingMixIn,
+                         BaseHTTPServer.HTTPServer):
+    """ ThreadedHTTPServer """
+    allow_reuse_address = True
+
+
+def httpserve(ipaddr, serve_port):
+    """ httpserve """
+    try:
+        if ipaddr is '':
+            raise AppException('get local ip exp')
+        if int(serve_port) <= 0 or int(serve_port) > 65535:
+            raise AppException('port num err')
+    except AppException as ex:
+        APPLOGGER.error(ex)
+    APPLOGGER.info('Server Up IP=%s PORT=%s', ipaddr, serve_port)
+    server = None
+    try:
+        server = ThreadedHTTPServer((ipaddr, serve_port), HttpHandler)
+    except socket.error as ex:
+        APPLOGGER.error(ex)
+        sys.exit(1)
+    if server:
+        server.serve_forever()
+    else:
+        APPLOGGER.error('http server start error')
+
+class HybirdServer(object):
+    """ HybirdServer """
+    def __init__(self):
+        self.__callbacklistdict = {}
+        self.thrlist = []
+    def setservices(self, name, callback, params):
+        """ set services and params """
+        self.__callbacklistdict[name] = [callback, params]
+    def __preserve(self):
+        """ preserve """
+        self.thrlist = []
+        for name in self.__callbacklistdict.keys():
+            _cbinfo = self.__callbacklistdict[name]
+            # quick and dirty
+            _tmpthr = threading.Thread(target=_cbinfo[0], args=_cbinfo[1])
+            _tmpthr.setDaemon(True)
+            _tmpthr.setName(name)
+            self.thrlist.append(_tmpthr)
+    def serve(self):
+        """ serve all """
+        self.__preserve()
+        for item in self.thrlist:
+            item.start()
+        # not block join
+        while 1:
+            for item in self.thrlist:
+                if item is not None and item.isAlive():
+                    item.join(1)
+
+def main():
+    """ serve for all """
+    hyserve = HybirdServer()
+    # you can start tcp server or http server
+    try:
+        local_ip = __get_local_ip()
+        tcpctl_port = 9999
+        http_port = 8080
+        if local_ip == '':
+            raise AppException('local ip is empty')
+        hyserve.setservices('httpserver', httpserve,
+                            (__get_local_ip(), http_port))
+        hyserve.setservices('tcpserver', tcpserve,
+                            (__get_local_ip(), tcpctl_port))
+        hyserve.serve()
+    except AppException as ex:
+        APPLOGGER.error(ex)
+
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
-    tcpserve()
+    main()
